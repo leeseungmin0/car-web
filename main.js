c9196e9dc346cf66cf0141314e98b9c2cf68e666
leeseungmin0 <tmdalsdl112@naver.com>
2022-04-06 19:54:18 +0900

    FEAT : car model filter

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 "use strict";
 
-const modelBtnContainer = document.querySelector(".model__container");
-const modelmenuContainer = document.querySelector(".model__kind");
+const modelmenuContainer = document.querySelector(".model__kind ul");
 const cars = document.querySelectorAll(".car");
-modelBtnContainer.addEventListener("click", (e) => {
+modelmenuContainer.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter === null) {
     return;

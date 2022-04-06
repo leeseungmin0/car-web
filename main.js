3331a272fb663073f671e898e6150b8088db522d
leeseungmin0 <tmdalsdl112@naver.com>
2022-04-06 21:59:26 +0900

    FEAT : car model filter animation add

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 "use strict";
 
-const modelmenuContainer = document.querySelector(".model__kind ul");
+const carContainer = document.querySelector(".car__container");
+const modelMenu = document.querySelector(".model__kind ul");
 const cars = document.querySelectorAll(".car");
-modelmenuContainer.addEventListener("click", (e) => {
+modelMenu.addEventListener("click", (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter === null) {
     return;
   }
-  cars.forEach((car) => {
-    if (filter === "*" || filter === car.dataset.type) {
-      car.classList.remove("invisible");
-    } else {
-      car.classList.add("invisible");
-    }
-  });
-  console.log(filter);
+  carContainer.classList.add("car__animation");
+  setTimeout(() => {
+    cars.forEach((car) => {
+      if (filter === "*" || filter === car.dataset.type) {
+        car.classList.remove("invisible");
+      } else {
+        car.classList.add("invisible");
+      }
+    });
+
+    carContainer.classList.remove("car__animation");
+  }, 300);
 });
